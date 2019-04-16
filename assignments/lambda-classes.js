92c7cb52bc587489855ff270245d7667586d8f9a
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-04-16 17:58:39 -0500

    mvp DONE

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,40 @@
 // CODE here for your Lambda Classes
+
+
+class VideoGame {
+    constructor (attributes) {
+        this.name = attributes.name;
+        this.createdAt = new Date();
+        this.dimensions = attributes.dimensions;
+    }
+    destroy () {
+         return `${this.name} was removed from the game.`
+
+    }
+
+}
+
+class CharacterStats {
+
+
+    constructor (attributes) {
+        this.healthPoints = attributes.healthPoints
+    }
+
+    takeDamage () {
+        return `${this.name} was removed from the game.`
+    }
+}
+
+class Humanoid {
+    constructor (attributes) {
+        this.team = attibutes.team
+        this.weapons = attributes.weapons 
+        this.language = attributes.language
+    }
+
+    greet () {
+        return `${this.name} offers a greeting in ${this.language}.`
+
+    }
+}
